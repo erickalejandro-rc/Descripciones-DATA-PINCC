fdfb670ba19885c2b949fc24eff31aa18a754958
https://github.com/erickalejandro-rc/Acerca-de-DATA-PINCC.git <ErickAlejandro@ciencias.unam.mx>
2026-02-26 14:51:14 -0600

    Cabios 26/02/2026

--- main.js
+++
@@ -1,5 +0,0 @@
-function toggle(element) {
-        const descripcion = element.nextElementSibling;
-        descripcion.style.display =
-            descripcion.style.display === "block" ? "none" : "block";
-    }
